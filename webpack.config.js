3b640145f058bc487db5214b820e642f9a7ff0e3
Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>
2018-11-26 22:06:18 +0300

    feat: configure devServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,24 @@ const path = require('path');
 module.exports = {
   mode:'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true
+  },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname,'dist')
+    path: path.resolve(__dirname, 'dist')
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use:{
-          loader:'babel-loader?cacheDirectory=true',
-          options:{
-            presets:['@babel/preset-env']
+        use: {
+          loader: 'babel-loader?cacheDirectory=true',
+          options: {
+            presets: ['@babel/preset-env']
           }
         }
       }
