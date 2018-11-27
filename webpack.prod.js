1073c676555f8bdc919a4bf2c2f1e8683a115718
Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>
2018-11-27 20:18:54 +0300

    feat: add minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = merge(common,{
+module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin({
+      cache: true
+    })]
+  }
 });
