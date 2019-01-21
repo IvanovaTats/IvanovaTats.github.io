c829305016bfeccb370367ff2c9455151c9fb249
Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>
2019-01-21 21:15:57 +0300

    feat: get articles by Id
    Signed-off-by: Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const fs = require('fs');
-const file = './articles.json';
+const file = './data/articles.json';
 
 router.get('/', function (req, res, next) {
   fs.readFile(file, function read(err, content) {
@@ -14,5 +14,21 @@ router.get('/', function (req, res, next) {
     res.send(json);
   })
 });
+
+router.get('/:id', function (req, res, next) {
+  //let id = req.params.id;
+  fs.readFile(file, function read(err, content) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    let json = JSON.parse(content);
+    let article = json.articles.find((el, index,array) => { return el.id == req.params.id })
+    console.log(article);
+    res.send(article);
+  })
+});
+
+
 
 module.exports = router;
