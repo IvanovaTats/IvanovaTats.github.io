9c091789389975f8b25b700227e11ed95d385bcb
Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>
2019-01-21 22:38:11 +0300

    feat: post added
    Signed-off-by: Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const fs = require('fs');
 const file = './data/articles.json';
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   fs.readFile(file, function read(err, content) {
     if (err) {
       console.log(err);
@@ -15,20 +15,43 @@ router.get('/', function (req, res, next) {
   })
 });
 
-router.get('/:id', function (req, res, next) {
-  //let id = req.params.id;
+router.get('/:id', (req, res, next) => {
   fs.readFile(file, function read(err, content) {
     if (err) {
       console.log(err);
       throw err;
     }
     let json = JSON.parse(content);
-    let article = json.articles.find((el, index,array) => { return el.id == req.params.id })
-    console.log(article);
+    let article = json.articles.find((el, index, array) => { return el.id == req.params.id })
     res.send(article);
   })
 });
 
+router.post('/', (req, res) => {
+  const article = req.body;
+  fs.readFile(file, function read(err, content) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    let jsonObject = JSON.parse(content);
+    jsonObject.articles.push(article);
 
+    json = JSON.stringify(jsonObject);
+    fs.writeFile(file, json, 'utf8', (err) => {
+      if (err) throw err;
+      console.log('complete');
+    });
+  })
+  res.status(201).send();
+});
+
+router.put('/', (req, res) => {
+  res.send('put');
+});
+
+router.delete('/', (req, res) => {
+  res.send('delete');
+});
 
 module.exports = router;
